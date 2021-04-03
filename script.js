e0e68eca6931d1d60ff271045ac83abd2e975494
Jihye Gim <77537852+GimWise@users.noreply.github.com>
2021-04-04 01:18:33 +0900

    body 완성, js 완성, navbar 링크 오류 수정

--- script.js
+++ script.js
@@ -20,3 +20,26 @@ document.getElementById('days').innerHTML = Math.floor(distance / (day)),
   document.getElementById('seconds').innerHTML = Math.floor((distance % (minute)) / second);
 
 }, second)
+
+var modal = document.querySelector(".modal"); 
+var trigger = document.querySelector(".home__aply"); 
+var closeButton = document.querySelector(".close-button"); 
+var submitButton = document.querySelector("#submit");
+
+console.log(modal);
+
+function toggleModal() { 
+    modal.classList.toggle("show-modal"); 
+}
+
+function windowOnClick(event) { 
+    if (event.target === modal) { 
+        toggleModal(); 
+    } 
+}
+
+trigger.addEventListener("click", toggleModal); 
+closeButton.addEventListener("click", toggleModal); 
+
+submit.addEventListener("click", toggleModal); 
+window.addEventListener("click", windowOnClick); 
